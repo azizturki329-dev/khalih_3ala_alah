5cb8ae429a6c477d01b719207d4c9c60059c60f5
Zaynebbouali <ZaIneb.BOUALI@esprit.tn>
2026-03-25 12:46:46 +0100

    feat: ajout validation formulaire login

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+const form = document.querySelector("form");
+
+
+    const email = form.querySelector('input[type="email"]').value;
+    const password = form.querySelector('input[type="password"]').value;
+
+
+    if (email === "" || password === "") {
+        alert("Veuillez remplir tous les champs !");
+        return;
+    }
+
+
+    if (password.length < 6) {
+        alert("Le mot de passe doit contenir au moins 6 caractères !");
+        return;
+    }
+
+
+    alert(`Bienvenue !\nEmail : ${email}`);
+
+
+    form.reset();
+    const btn = document.querySelector("button");
+btn.addEventListener("mouseover", function(){
+    btn.style.backgroundColor = "lightblue";
+});
+btn.addEventListener("mouseout", function(){
+    btn.style.backgroundColor = "";
+});
+});
