baf391927d5832d3c555e2c7ba93312c08f65a1e
Zaynebbouali <ZaIneb.BOUALI@esprit.tn>
2026-03-27 12:29:26 +0100

    fix: button hover style reset closes #14

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ btn.addEventListener("mouseover", () => {
 });
 
 btn.addEventListener("mouseout", () => {
-    btn.style.backgroundColor = "";
+    btn.style.backgroundColor = "initial";
 });
 
 // Submit form
